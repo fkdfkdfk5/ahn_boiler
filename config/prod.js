a08ca9a4514c002096904cc25fb678d45048eb38
fkdfkdfk5 <fkdfkdfk5@gmail.com>
2021-08-11 17:17:44 +0900

    비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  //이건 헤로쿠 배포할때 헤로쿠 가입한후해야하는데 다시보든가 하자
+};
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://dp81:1111@cluster0.3knpe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("Mongdb Connected"))
   .catch((err) => console.log(err));
 
@@ -20,5 +23,16 @@ mongoose
 // 아이디 dp81 1111 이다
 
 app.get("/", (req, res) => res.send("hello"));
+
+app.post("/register", (req, res) => {
+  //클라이언트에서 보낸것을 바디파서가 req.body 로 변환해서 보냄
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
